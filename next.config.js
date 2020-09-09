c35da5092937684e36521243fcacaea7cf7bc8c7
Sashank Thupukari <s@thupukari.com>
2020-09-08 20:10:56 -0400

    Fix tailwind css purge config

--- next.config.js
+++ next.config.js
@@ -100,15 +100,17 @@ module.exports = withBundleAnalyzer({
       ],
     })
 
-//     if (!options.dev && options.isServer) {
-//       const originalEntry = config.entry
+    if (!options.dev && options.isServer) {
+      const originalEntry = config.entry
 
-//       config.entry = async () => {
-//         const entries = { ...(await originalEntry()) }
-//         entries['./scripts/build-rss.js'] = './scripts/build-rss.js'
-//         return entries
-//       }
-//     }
+      config.entry = async () => {
+        const entries = { ...(await originalEntry()) }
+
+        // TODO: Add rss back
+        // entries['./scripts/build-rss.js'] = './scripts/build-rss.js'
+        return entries
+      }
+    }
 
     return config
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const mdx = require("@mdx-js/mdx");
+const defaultTheme = require('tailwindcss/defaultTheme')
+const mdx = require('@mdx-js/mdx')
 
 module.exports = {
   purge: {
-    mode: "all",
-    content: ["./src/**/*.{js,mdx}", "./next.config.js"],
+    mode: 'all',
+    content: [
+      './pages/**/*.{js,tsx,ts,mdx}',
+      './components/**/*.{js,tsx,ts,mdx}',
+      './next.config.js',
+    ],
     options: {
       extractors: [
         {
-          extensions: ["mdx"],
+          extensions: ['mdx'],
           extractor: (content) => {
-            content = mdx.sync(content);
+            content = mdx.sync(content)
 
             // Capture as liberally as possible, including things like `h-(screen-1.5)`
-            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
 
             // Capture classes within other delimiters like .block(class="w-1/2") in Pug
             const innerMatches =
-              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) ||
-              [];
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
 
-            return broadMatches.concat(innerMatches);
+            return broadMatches.concat(innerMatches)
           },
         },
       ],
@@ -29,57 +32,57 @@ module.exports = {
   theme: {
     extend: {
       lineHeight: {
-        "11": "2.75rem",
-        "12": "3rem",
-        "13": "3.25rem",
-        "14": "3.5rem",
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
       },
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         code: {
-          green: "#b5f4a5",
-          yellow: "#ffe484",
-          purple: "#d9a9ff",
-          red: "#ff8383",
-          blue: "#93ddfd",
-          white: "#fff",
+          green: '#b5f4a5',
+          yellow: '#ffe484',
+          purple: '#d9a9ff',
+          red: '#ff8383',
+          blue: '#93ddfd',
+          white: '#fff',
         },
       },
       typography: (theme) => ({
         default: {
           css: {
-            color: theme("colors.gray.700"),
+            color: theme('colors.gray.700'),
             h2: {
-              fontWeight: "700",
-              letterSpacing: theme("letterSpacing.tight"),
-              color: theme("colors.gray.900"),
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.gray.900'),
             },
             h3: {
-              fontWeight: "600",
-              color: theme("colors.gray.900"),
+              fontWeight: '600',
+              color: theme('colors.gray.900'),
             },
-            "ol li:before": {
-              fontWeight: "600",
-              color: theme("colors.gray.500"),
+            'ol li:before': {
+              fontWeight: '600',
+              color: theme('colors.gray.500'),
             },
-            "ul li:before": {
-              backgroundColor: theme("colors.gray.400"),
+            'ul li:before': {
+              backgroundColor: theme('colors.gray.400'),
             },
             code: {
-              color: theme("colors.gray.900"),
+              color: theme('colors.gray.900'),
             },
             a: {
-              color: theme("colors.gray.900"),
+              color: theme('colors.gray.900'),
             },
             pre: {
-              color: theme("colors.gray.200"),
-              backgroundColor: theme("colors.gray.800"),
+              color: theme('colors.gray.200'),
+              backgroundColor: theme('colors.gray.800'),
             },
             blockquote: {
-              color: theme("colors.gray.900"),
-              borderLeftColor: theme("colors.gray.200"),
+              color: theme('colors.gray.900'),
+              borderLeftColor: theme('colors.gray.200'),
             },
           },
         },
@@ -88,33 +91,31 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    require("@tailwindcss/ui"),
-    require("@tailwindcss/typography"),
+    require('@tailwindcss/ui'),
+    require('@tailwindcss/typography'),
     function ({ addBase, addComponents, theme }) {
       addBase([
         {
-          "@font-face": {
-            fontFamily: "Inter var",
-            fontWeight: "100 900",
-            fontStyle: "normal",
-            fontNamedInstance: "Regular",
-            fontDisplay: "swap",
-            src:
-              'url("/fonts/Inter-roman.var-latin.woff2?3.13") format("woff2")',
+          '@font-face': {
+            fontFamily: 'Inter var',
+            fontWeight: '100 900',
+            fontStyle: 'normal',
+            fontNamedInstance: 'Regular',
+            fontDisplay: 'swap',
+            src: 'url("/fonts/Inter-roman.var-latin.woff2?3.13") format("woff2")',
           },
         },
         {
-          "@font-face": {
-            fontFamily: "Inter var",
-            fontWeight: "100 900",
-            fontStyle: "italic",
-            fontNamedInstance: "Italic",
-            fontDisplay: "swap",
-            src:
-              'url("/fonts/Inter-italic.var-latin.woff2?3.13") format("woff2")',
+          '@font-face': {
+            fontFamily: 'Inter var',
+            fontWeight: '100 900',
+            fontStyle: 'italic',
+            fontNamedInstance: 'Italic',
+            fontDisplay: 'swap',
+            src: 'url("/fonts/Inter-italic.var-latin.woff2?3.13") format("woff2")',
           },
         },
-      ]);
+      ])
     },
   ],
-};
+}
