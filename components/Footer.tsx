3251660ba34b7a94e37e371e51297b852388f7c7
Sashank Thupukari <s@thupukari.com>
2020-09-10 11:32:33 -0400

    New footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,64 +1,64 @@
 import * as React from 'react'
-import { useRouter } from 'next/router'
-import cx from 'classnames'
 
 const Footer = () => {
-  const router = useRouter()
-
   return (
-    <footer style={{height: 357, paddingTop: 20, borderTop: '0px solid #D6E3ED', background: 'url(images/oceandagster.png), #f5f9fc', backgroundSize: 'cover', backgroundPosition: '90% 0%'}}>
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <div className="flex">
-            <div className="flex-shrink-0 flex items-center">
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://docs.dagster.io/assets/logos/small.png"
-                alt="Dagster logo"
-              />
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src="https://docs.dagster.io/assets/logos/small.png"
-                alt="Dagster logo"
-              />
-              <div className="font-bold text-xl ml-3">Dagster</div>
-            </div>
-            <div className="hidden sm:ml-6 sm:flex">
-              <a
-                href="/"
-                className={cx(
-                  'ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out',
-                  {
-                    'border-indigo-500': !router.pathname.startsWith('/blog'),
-                  }
-                )}
-              >
+    <footer
+      style={{
+        height: 357,
+        paddingTop: 20,
+        borderTop: '0px solid #D6E3ED',
+        background: 'url(images/oceandagster.png), #f5f9fc',
+        backgroundSize: 'cover',
+        backgroundPosition: '90% 0%',
+      }}
+    >
+      <div className="">
+        <div className="max-w-screen-xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+          <nav className="-mx-5 -my-2 flex flex-wrap justify-center">
+            <div className="px-5 py-2">
+              <a href="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                 Home
               </a>
-              <a
-                href="/blog"
-                className={cx(
-                  'ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out',
-                  {
-                    'border-indigo-500': router.pathname.startsWith('/blog'),
-                  }
-                )}
-              >
+            </div>
+            <div className="px-5 py-2">
+              <a href="/blog" className="text-base leading-6 text-gray-500 hover:text-gray-900">
                 Blog
               </a>
+            </div>
+            <div className="px-5 py-2">
               <a
-                href="#"
-                className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                href="https://docs.dagster.io/"
+                className="text-base leading-6 text-gray-500 hover:text-gray-900"
               >
                 Docs
               </a>
-              <a
-                href="#"
-                className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-              >
-                Github
-              </a>
             </div>
+          </nav>
+          <div className="mt-8 flex justify-center">
+            <a href="#" className="ml-6 text-gray-400 hover:text-gray-500">
+              <span className="sr-only">GitHub</span>
+              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path
+                  fillRule="evenodd"
+                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+
+            <a href="#" className="ml-6 text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Slack</span>
+              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.528 2.528 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.528 2.528 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.522v-6.313zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.522v2.52H8.834zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V2.522A2.527 2.527 0 0 1 15.165 0a2.528 2.528 0 0 1 2.523 2.522v6.312zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.522A2.528 2.528 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.522h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z" />
+              </svg>
+            </a>
+
+            <a href="#" className="ml-6 text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Stackoverflow</span>
+              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M18.986 21.865v-6.404h2.134V24H1.844v-8.539h2.13v6.404h15.012zM6.111 19.731H16.85v-2.137H6.111v2.137zm.259-4.852l10.48 2.189.451-2.07-10.478-2.187-.453 2.068zm1.359-5.056l9.705 4.53.903-1.95-9.706-4.53-.902 1.936v.014zm2.715-4.785l8.217 6.855 1.359-1.62-8.216-6.853-1.35 1.617-.01.001zM15.751 0l-1.746 1.294 6.405 8.604 1.746-1.294L15.749 0h.002z" />
+              </svg>
+            </a>
           </div>
         </div>
       </div>
