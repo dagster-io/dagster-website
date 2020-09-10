da5ccb27c6128bafeabeb986594b2ec790b9c17c
Sashank Thupukari <s@thupukari.com>
2020-09-10 11:16:44 -0400

    New header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
-            <div className="flex-shrink-0 flex items-center">
+            <a href="/" className="flex-shrink-0 flex items-center">
               <img
                 className="block lg:hidden h-8 w-auto"
                 src="https://docs.dagster.io/assets/logos/small.png"
@@ -24,7 +24,7 @@ const Header = () => {
                 alt="Dagster logo"
               />
               <div className="font-bold text-xl ml-3">Dagster</div>
-            </div>
+            </a>
             <div className="hidden sm:ml-6 sm:flex">
               <a
                 href="/"
@@ -49,33 +49,25 @@ const Header = () => {
                 Blog
               </a>
               <a
-                href="#"
+                href="https://docs.dagster.io"
                 className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
               >
                 Docs
               </a>
-              <a
-                href="#"
-                className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-              >
-                Github
-              </a>
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
-            <button
-              className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
-              aria-label="Notifications"
-            >
-              <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-            </button>
+            <div className={cx('flex flex-row nowrap justify-around w-40')}>
+              <a href="https://github.com/dagster-io/dagster">
+                <img className="h-6" src="/images/logos/github-icon.svg" />
+              </a>
+              <a href="https://dagster-slackin.herokuapp.com/">
+                <img className="h-6" src="/images/logos/slack-icon.svg" />
+              </a>
+              <a href="https://stackoverflow.com/questions/tagged/dagster">
+                <img className="h-6" src="/images/logos/stack-overflow-icon.svg" />
+              </a>
+            </div>
           </div>
           <div className="-mr-2 flex items-center sm:hidden">
             {/* Mobile menu button */}
