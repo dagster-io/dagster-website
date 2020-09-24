15bd6ba36490ec6f5e978b84127c7857aa919306
yuhan <yuhan@elementl.com>
2020-09-23 21:54:12 -0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,37 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const mdx = require('@mdx-js/mdx')
 
 module.exports = {
-  purge: {
-    mode: 'all',
-    content: [
-      './pages/**/*.{js,tsx,ts,mdx}',
-      './components/**/*.{js,tsx,ts,mdx}',
-      './next.config.js',
-    ],
-    options: {
-      extractors: [
-        {
-          extensions: ['mdx'],
-          safelist: {
-            standard: [/^bg-/, /^text-/],
-          },
-          extractor: (content) => {
-            content = mdx.sync(content)
+  purge: false,
+  // {
+  //   mode: 'all',
+  //   content: [
+  //     './pages/**/*.{js,tsx,ts,mdx}',
+  //     './components/**/*.{js,tsx,ts,mdx}',
+  //     './next.config.js',
+  //   ],
+  //   options: {
+  //     extractors: [
+  //       {
+  //         extensions: ['mdx'],
+  //         safelist: {
+  //           standard: [/^bg-/, /^text-/],
+  //         },
+  //         extractor: (content) => {
+  //           content = mdx.sync(content)
 
-            // Capture as liberally as possible, including things like `h-(screen-1.5)`
-            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+  //           // Capture as liberally as possible, including things like `h-(screen-1.5)`
+  //           const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
 
-            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-            const innerMatches =
-              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+  //           // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+  //           const innerMatches =
+  //             content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
 
-            return broadMatches.concat(innerMatches)
-          },
-        },
-      ],
-    },
-  },
+  //           return broadMatches.concat(innerMatches)
+  //         },
+  //       },
+  //     ],
+  //   },
+  // },
   theme: {
     extend: {
       lineHeight: {
