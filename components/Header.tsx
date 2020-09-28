f4d4e035d5bb50598fb9ab03571a7bf2a690a497
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2020-09-28 10:24:08 -0700

    add "API Docs" to header (#22)

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,6 +54,13 @@ const Header = () => {
               >
                 Docs
               </a>
+
+              <a
+                href="https://docs.dagster.io/_apidocs"
+                className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+              >
+                API Docs
+              </a>
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
