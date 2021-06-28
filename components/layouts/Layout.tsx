210c524ee0122f44b9667bb6454f5a6f47ef49b0
catherinewu <cat@elementl.com>
2021-06-28 14:16:38 -0400

    fix: add padding top to blog

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -38,7 +38,10 @@ const Layout = ({ children, title = 'Dagster', description = DEFAULT_SHARE_DESCR
     </Head>
 
     <Header />
-    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div 
+      className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8"
+      style={{ paddingTop: '64px' }}
+    >
       <div className="flex">{children}</div>
     </div>
   </div>
