e6ed4adc516a13939da5787afa1ae5ba8011b75a
catherinewu <cat@elementl.com>
2021-06-30 17:10:02 -0400

    test w/ https

---
+++ pages/cloud.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Widget } from '@typeform/embed-react'
+
+export default class Typeform extends React.Component {
+  render() {
+    return <Widget id="km4mFiDz" style={{ width: '100%' }} className="my-form" />
+  }
+}
