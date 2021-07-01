a83f5e04d775b7daa6b63b6c0e5d3fa131bacb9a
catherinewu <cat@elementl.com>
2021-07-01 19:27:53 -0400

    dagster waitlist mock

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,6 +67,12 @@ const Header = () => {
                 >
                   Docs
                 </a>
+                <a
+                  href="/cloud"
+                  className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                >
+                  Cloud
+                </a>
               </div>
             </div>
             <div className="hidden space-x-6 sm:ml-6 sm:flex sm:items-center">
---
+++ pages/cloud.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+
+export default class Typeform extends React.Component {
+  render() {
+    return (
+      <div style={{ width: '100%', paddingTop: '80px', paddingBottom: '80px' }}>
+        <div style={{ marginBottom: '40px' }}>
+            <p style={{ color: '#444444', textAlign: 'center', fontSize: '48px', fontWeight: 'bold', marginBottom: '16px' }}>
+            Dagster Cloud
+            </p>
+            <p style={{  color: '#7b88a8', textAlign: 'center', fontSize: '18px', fontWeight: 400, lineHeight: 1.45, marginBottom: '18px' }}>
+            Turn your modern data stack into a modern data platform with the hosted version of Dagster.
+            </p>
+            <div style={{ display: 'flex', justifyContent: 'center', color: '#7b88a8', textAlign: 'center', fontSize: '16px', fontWeight: 400, lineHeight: 1.45, marginBottom: '18px' }}>
+                <div style={{ width: '200px', margin: '2%' }}>
+                <img src="images/fast.svg"/>
+                <p>Fast and lightweight solution for running Dagster without the infrastructure ops</p>
+                </div>
+                <div style={{ width: '200px', margin: '2%' }}>
+                <img src="images/cloud.svg"/>
+                <p>Fully-hosted Dagster components, including Dagit, scheduler, metadata databases</p>
+                </div>
+                <div style={{ width: '200px', margin: '2%' }}>
+                <img src="images/rbac.svg" />
+                <p>Enterprise-grade features, including access control, run attribution, audit logs, pipeline monitoring</p>
+                </div>
+                <div style={{ width: '200px', margin: '2%' }}>
+                <img src="images/security.svg"/>
+                <p>Your data is always stored by you</p>
+                </div>
+            </div>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <a
+            href="https://ppho1i7017a.typeform.com/to/km4mFiDz"
+            target="_blank"
+            style={{ backgroundColor: '#519BE7', padding: '16px', fontSize: '16px', fontWeight: 500, color: '#ffffff', borderRadius: '5px' }}
+          >
+            Sign up for the beta
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
