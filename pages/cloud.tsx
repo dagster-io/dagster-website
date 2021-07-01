e262064ac4a24d639126b4dc227360156537c6c8
catherinewu <cat@elementl.com>
2021-06-30 22:04:11 -0400

    styling

--- pages/cloud.tsx
+++ pages/cloud.tsx
@@ -3,6 +3,12 @@ import { Widget } from '@typeform/embed-react'
 
 export default class Typeform extends React.Component {
   render() {
-    return <Widget id="km4mFiDz" style={{ width: '100%' }} className="my-form" />
+    return (
+    <div style={{ width: '100%', paddingTop: '16px', paddingBottom: '16px'}}>
+    <h1 style={{ color: 'black', textAlign: 'center', fontSize: '48px', fontWeight: 'bold' }}>Dagster Cloud</h1>
+    <h1 style={{ color: 'black', textAlign: 'center', fontSize: '32px' }}>Turn your modern data stack into a modern data platform</h1>
+    <Widget id="km4mFiDz" style={{ width: '100%', height: '500px' }} className="my-form" />
+    </div>
+    )
   }
 }
