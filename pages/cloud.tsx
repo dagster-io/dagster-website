c2b4027fa23e67dc69774d5f5e28b7056c40e0e3
catherinewu <cat@elementl.com>
2021-07-06 20:27:34 -0400

    small fixes and re-word

--- pages/cloud.tsx
+++ pages/cloud.tsx
@@ -5,31 +5,31 @@ export default class Typeform extends React.Component {
     return (
       <div style={{ width: '100%', paddingTop: '80px', paddingBottom: '80px' }}>
         <div style={{ marginBottom: '40px' }}>
-            <p style={{ color: '#444444', textAlign: 'center', fontSize: '48px', fontWeight: 'bold', marginBottom: '16px' }}>
+            <p style={{ color: '#1F2023', textAlign: 'center', fontSize: '48px', fontWeight: 'bold', marginBottom: '16px' }}>
             Dagster Cloud
             </p>
-            <p style={{  color: '#7b88a8', textAlign: 'center', fontSize: '18px', fontWeight: 400, lineHeight: 1.45, marginBottom: '18px' }}>
+            <p style={{  color: '#383B40', textAlign: 'center', fontSize: '18px', fontWeight: 400, lineHeight: 1.45, marginBottom: '18px' }}>
             Turn your modern data stack into a modern data platform with hosted Dagster
             </p>
-            <div style={{ display: 'flex', justifyContent: 'center', color: '#7b88a8', textAlign: 'center', fontSize: '16px', fontWeight: 400, lineHeight: 1.45, marginBottom: '18px' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', color: '#383B40', textAlign: 'center', fontSize: '16px', fontWeight: 400, lineHeight: 1.45, marginBottom: '18px' }}>
                 <div style={{ width: '200px', margin: '2%', justifyContent: 'start', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 <img style={{ width: '70%' }} src="images/cloud.svg"/>
-                <p>Fully-hosted Dagster components, including Dagit, scheduler, metadata databases</p>
+                <p>Fully-hosted Dagster components including Dagit, scheduler, metadata databases</p>
                 </div>
 
                 <div style={{ width: '200px', margin: '2%', justifyContent: 'start', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 <img style={{ width: '70%' }} src="images/rbac.svg" />
-                <p>Enterprise-grade features, including access control, run attribution, audit logs, pipeline monitoring</p>
+                <p>Enterprise-grade features including access control, run attribution, audit logs, pipeline monitoring</p>
                 </div>
 
                 <div style={{ width: '200px', margin: '2%', justifyContent: 'start', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 <img style={{ width: '70%' }} src="images/fast.svg"/>
-                <p>Fast and lightweight way to run Dagster with limited infrastructure ops</p>
+                <p>Simple and lightweight to use, so your data team can productionize pipelines faster</p>
                 </div>
 
                 <div style={{ width: '200px', margin: '2%', justifyContent: 'start', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 <img style={{ width: '70%' }} src="images/security.svg"/>
-                <p>We value data security, and your data is stored with you</p>
+                <p>We value security, so your data is stored by you and your business logic stays with you</p>
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@ export default class Typeform extends React.Component {
           <a
             href="https://ppho1i7017a.typeform.com/to/km4mFiDz"
             target="_blank"
-            style={{ backgroundColor: '#519BE7', padding: '16px', fontSize: '16px', fontWeight: 500, color: '#ffffff', borderRadius: '5px' }}
+            style={{ border: '1px solid #336ea5', backgroundColor: '#4485c2', padding: '16px', fontSize: '16px', fontWeight: 500, color: '#ffffff', borderRadius: '5px' }}
           >
             Join Dagster Cloud Beta
           </a>
