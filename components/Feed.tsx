46c58d5e86cf23b0b6e087db9556abf69cb22116
yuhan <yuhan@elementl.com>
2021-07-09 11:43:00 -0700

    blog tags 1/ User Stories page

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -32,3 +32,35 @@ export const FeedCard: React.FunctionComponent<any> = ({ post }) => {
     </div>
   )
 }
+
+function truncate(text: string, max: number) {
+  return text.length > max ? text.slice(0, max).split(' ').slice(0, -1).join(' ') + ' ...' : text
+}
+
+export const GridCard: React.FunctionComponent<any> = ({ post }) => {
+  const { slug, title, excerpt, coverImage, date } = post
+
+  return (
+    <div
+      key={slug}
+      className="transition duration-200 ease-in-out transform hover:-translate-y-2 shadow-md hover:shadow-lg mb-4"
+    >
+      <NextLink href={post.href}>
+        <a className="hover:text-blue-600" aria-label={`Read "${title}"`}>
+          <div>{coverImage && <img src={coverImage} />}</div>
+          <div className="py-4 px-8">
+            <h3 className="text-xl mb-4 font-bold tracking-tight">
+              <NextLink href={post.href}>
+                <a className="hover:text-blue-600">{post.title}</a>
+              </NextLink>
+            </h3>
+            <div className="text-sm max-w-none text-gray-500 mb-4">{truncate(excerpt, 180)}</div>
+            <dl>
+              <dd className="text-xs text-gray-700">{date}</dd>
+            </dl>
+          </div>
+        </a>
+      </NextLink>
+    </div>
+  )
+}
