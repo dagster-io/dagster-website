b2421966fcb8edf41d7b5db8437cce17ba202dc0
catherinewu <cat@elementl.com>
2021-07-13 09:25:23 -0400

    up

--- pages/cloud.tsx
+++ pages/cloud.tsx
@@ -6,13 +6,13 @@ export default class Typeform extends React.Component {
     return (
       <div className="flex flex-col p-16 m-24 bg-gradient-to-r from-blue-200 to-blue-300 rounded-md">
         <p className="text-blue-900 text-2xl font-bold flex justify-center pb-2">Dagster Cloud</p>
-        <p className="text-blue-900 medium flex justify-left">
-          We are building a hosted version of Dagster and are accepting waitlist sign ups! We look
-          forward to partnering with you to make Dagster Cloud the fastest way for teams to
-          orchestrate and operate data pipelines.
+        <p className="text-blue-900 font-medium flex justify-left pb-2">
+        We are building a hosted product to make deploying and operating Dagster fast, secure, and effortless.
+        </p>
+        <p className="text-blue-900 font-light flex justify-left">
+        Sign up below to get on the waitlist and stay informed of our progress.
         </p>
         <div className="flex justify-center">
-          {/* <JotformEmbed src="https://form.jotform.com/211927952953163" /> */}
           <JotformEmbed src="https://form.jotform.com/211927287192159" />
         </div>
       </div>
