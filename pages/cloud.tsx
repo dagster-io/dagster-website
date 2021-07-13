985152e96bcb0c5c168dc13498e94b69606dd8e0
catherinewu <cat@elementl.com>
2021-07-12 22:23:23 -0400

    up

--- pages/cloud.tsx
+++ pages/cloud.tsx
@@ -4,14 +4,12 @@ import JotformEmbed from 'react-jotform-embed';
 export default class Typeform extends React.Component {
   render() {
     return (
-      <div>
-      <div className="flex flex-col p-16 m-24 border-2 border-blue-100 bg-blue-100 rounded-md rounded-md">
-      <p>We are excited to share that we are building a hosted version of the Dagster and are looking for early beta users!</p>
+      <div className="flex flex-col p-16 m-24 bg-gradient-to-r from-blue-200 to-blue-300 rounded-md">
+      <p className="text-blue-900 text-2xl flex justify-center pb-2">Dagster Cloud</p>
+      <p className="text-blue-900 flex justify-center">We are excited to share that we are building a hosted version of the Dagster and are looking for early beta users!</p>
       <div className="flex justify-center">
-        {/* style={{ display: 'flex', justifyContent: 'center', height: '200px' }} */}
         {/* <JotformEmbed src="https://form.jotform.com/211927952953163" /> */}
         <JotformEmbed src="https://form.jotform.com/211927287192159" />
-      </div>
       </div>
       </div>
     )
