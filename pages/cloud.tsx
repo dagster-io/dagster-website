c3fd784f6e42004405f8a358c41cbb0870d69767
catherinewu <cat@elementl.com>
2021-07-12 21:53:46 -0400

    waitlist

---
+++ pages/cloud.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import JotformEmbed from 'react-jotform-embed';
+
+export default class Typeform extends React.Component {
+  render() {
+    return (
+      <div>
+      <div className="flex flex-col p-16 m-24 border-2 border-blue-100 bg-blue-100 rounded-md rounded-md">
+      <p>We are excited to share that we are building a hosted version of the Dagster and are looking for early beta users!</p>
+      <div className="flex justify-center">
+        {/* style={{ display: 'flex', justifyContent: 'center', height: '200px' }} */}
+        <JotformEmbed src="https://form.jotformeu.com/211927287192159" />
+      
+      </div>
+      </div>
+      </div>
+    )
+  }
+}
