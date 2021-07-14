714a82b7ef652a5d8d91c7fc3544f0b0f95c426c
Catherine Wu <cat@elementl.com>
2021-07-14 12:03:48 -0400

    dagster.io privacy notice (#74)
    add privacy notice to dagster.io/privacy

--- pages/cloud.tsx
+++ pages/cloud.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import JotformEmbed from 'react-jotform-embed'
 
-export default class Typeform extends React.Component {
+export default class Waitlist extends React.Component {
   render() {
     return (
       <div className="flex flex-col p-16 m-24 bg-gradient-to-r from-blue-200 to-blue-300 rounded-md">
@@ -10,9 +10,6 @@ export default class Typeform extends React.Component {
         We are building a hosted product to make deploying and operating Dagster fast, secure, and 
         effortless. Sign up below to get on the waitlist and stay informed of our progress.
         </p>
-        {/* <p className="text-blue-900 font-light flex justify-left">
-        Sign up below to get on the waitlist and stay informed of our progress.
-        </p> */}
         <div className="flex justify-center">
           <JotformEmbed src="https://form.jotform.com/211927287192159" />
         </div>
